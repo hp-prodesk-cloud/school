87309a295537076577128665dde31a578d856408
hp-prodesk-cloud <25benmcgrath.acc@lwetb.ie>
2026-03-25 15:04:48 +0000

    Add files via upload

---
+++ for_loop_revision_25_march.py
@@ -0,0 +1,16 @@
+a = input('enter an integer')
+b = input('enter an integer')
+c = input('enter an integer')
+a=int (a)
+b=int (b)
+c=int (c)
+a_squared= a * a
+b_squared= b * b
+c_squared= c * c
+a_squared = int (a_squared)
+b_squared = int (b_squared)
+c_squared = int (c_squared)
+if a_squared + b_squared == c_squared :
+    print ('the number you entered is a pythagorean triple')
+else :
+    print('the number you have entered is not a pythagorean triple')
