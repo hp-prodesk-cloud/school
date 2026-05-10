c173cd6400251e8621d5deb6dc1e36e1fb705ddf
hp-prodesk-cloud <25benmcgrath.acc@lwetb.ie>
2026-05-10 20:32:12 +0100

    Add files via upload

---
+++ codingclasshomework1.py
@@ -0,0 +1,13 @@
+#author ben mcgrath
+num_1 = input ('enter the sum of the first one of the two numbers in which will be used for the programe')
+num_2 = input ('enter the sum of the second one of the two numbers in which will be used for the programe')
+num_2 = float(num_2)
+num_1 = float(num_1)
+sol1 = num_1 + num_2
+print(sol1)
+sol2 = num_1 - num_2
+print(sol2)
+sol3 = num_1 * num_2
+print(sol3)
+sol4 = num_1 / num_2
+print(sol4)
---
+++ codingclasshomework2.py
@@ -0,0 +1,6 @@
+length = input('enter the length of the square')
+width = input('enter the width of the square')
+width = float(width)
+length = float(length)
+area = length * width
+print (area)
